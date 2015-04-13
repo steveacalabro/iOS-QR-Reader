b2814198375c142b8dd101ec4005b46659fa5730
Stephen Calabro <steve.a.calabro@gmail.com>
2015-04-13 13:45:08 -0400

    Initial Commit

---
+++ QRReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRReader
+//
+//  Created by Stephen Calabro on 4/13/15.
+//  Copyright (c) 2015 Stephen Calabro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRReader
+//
+//  Created by Stephen Calabro on 4/13/15.
+//  Copyright (c) 2015 Stephen Calabro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
